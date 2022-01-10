b70a0b3bfebfe3d0cdc3dbf91769e06b1232749c
Arun kumar <72678258+myselfarun1998@users.noreply.github.com>
2022-01-10 17:57:55 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+import React from "react";
+import SingleCommend from "./singleComponents";
+import profile1 from "./img/images.jpg";
+import profile2 from "./img/img.png";
+import profile3 from "./img/img2.jpg";
+import Home from "./componets/home";
+import Header from "./componets/header";
+import Footer from "./componets/footer";
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
